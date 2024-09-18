15ac5bde05e05dfb2b0e805ec83d3e4ee4312753
Derrick Furtado <derrick.furtado10@gmail.com>
2024-09-17 19:00:07 -0600

    finishing up the day

--- purchase_history.py
+++ purchase_history.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
+
 
 purchase_data = pd.read_csv('empirical_data/amazonhistory_df.csv')
 
-print(purchase_data.columns)
+# print(purchase_data.columns)
 
 # print(purchase_data)
 
@@ -21,3 +24,26 @@ purchase_year_2023 = purchase_data.loc[purchase_data["Order Date"].str[:4] == '2
 sorted_2023 = purchase_year_2023.sort_values('Unit Price', ascending=False)
 
 top_5_spend_2023 = sorted_2023.iloc[:5]
+
+
+print(purchase_year_2019.isna())
+
+dedup_purchase_year_2019 = purchase_year_2019.dropna()
+
+print(purchase_year_2019.duplicated())
+
+
+
+'''
+
+I've found that this data set needs to be cleaned before analysis using data frames because all the data types are strings
+
+'''
+
+
+# sn.countplot(x = 'Billing Address', data = purchase_data)
+# sn.barplot(x = 'Billing Address', y = "Total Owed", data = purchase_data)             
+''' data types still aren't right '''
+plt.show()
+
+
